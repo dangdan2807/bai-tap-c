aa665ff1ef378672dae5ec4aa189b85e9cdb62e3
dangdan2807 <dangdan2807@gmail.com>
2019-11-08 00:38:17 +0700

    update c/lab-2

---
+++ c/lab 2/bai12.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	float tk1, tk2, tk3, bt1, bt2, gk, ck, tbckt, tbcbt, tbc;
+	printf("Nhap diem 3 bai kiem tra thuong ki: ");
+	scanf("%f%f%f",  &tk1,  &tk2,  &tk3) ;
+	printf("Nhap diem 2 bai kiem tra bai tap: ");
+	scanf("%f%f",  &bt1,  &bt2);
+	printf("Nhap diem bai kiem tra giua ki: ");
+	scanf("%f",  &gk) ;
+	cout << "Nhap diem bai kien tra cuoi ki: ";
+	scanf("%f",  &ck);
+	tbckt= (2*((tk1+tk2+tk3)/3)+3*gk+5*ck)/10;
+	tbcbt= (bt1+bt2)/2;
+	tbc= (tbckt+2*tbcbt)/3;
+	printf("Diem TBC KT la: %f\n",  tbckt) ;
+	printf("Diem TBC BT la: %f",  tbcbt) ;
+	printf("Diem TBC la: %f",  tbc) ;
+	return 0;
+}
---
+++ c/lab 2/bai13.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int hh, mm, ss;
+	printf("Nhap giay, phut, gio: ");
+	scanf("%d%d%d", &ss, &mm, &hh);
+	if(ss<59 && ss>=0)
+		ss++;
+	else if(ss>=59)
+	{
+		ss=0;
+		if(mm <59 && mm>=0)
+			mm++;
+		else if(mm>=59)
+		{
+			mm=0;
+			if(hh>=23)
+				hh=0;
+			else
+				hh++;
+		}
+	}
+	printf("Dong ho hien thi sau 1 giay la: %d:%d:%d", hh, mm, ss);
+	return 0;
+}
