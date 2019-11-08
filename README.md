6544215bb1d31120e33b28d6621f9ac65dcb9b6c
dangdan2807 <dangdan2807@gmail.com>
2019-11-08 08:47:30 +0700

    update c/lab5 - switch

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-﻿# share bài tập C/C++
+﻿﻿# share bài tập C/C++
 Share bài tập C\C++ tham khảo. 
 
 - Hiện tại chỉ có C++. Ai muốn dùng(chạy code) thì thay lại chỗ lệnh:
-     - "cout <<" bằng "printf". 
-     - "cin >>" bằng scanf. 
-     - hoặc để như thế chạy luôn cũng được không sao cả.
-	 - Đã làm đến bài 70 thuộc lab 11 tổ chức mảng.
+  - "cout <<" bằng "printf". 
+  - "cin >>" bằng scanf. 
+  - hoặc để như thế chạy luôn cũng được không sao cả.
+  - Đã làm đến bài 70 thuộc lab 11 tổ chức mảng.
 - Mình sẽ cập nhật C sau khi có thời gian.
-     - Đã cập nhật tạm vài bài của: 
-		-lab 2.
-		-lab 3.
-		-lab 4. 
+  - Đã cập nhật tạm vài bài của: 
+    - lab 2
+    - lab 3
+    - lab 4
---
+++ c/lab5-switch/vd3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int a, chuc, dv;
+	cout <<"Nhap 1 so nguyen nam tu 10 -> 99: ";
+	cin >>a;
+	chuc=a/10;
+	dv=a%10;
+	switch(chuc)
+	{
+		case 1: cout <<"Muoi "; break;
+		case 2: cout <<"Hai Muoi "; break;
+		case 3: cout <<"Ba muoi "; break;
+		case 4: cout <<"Bon muoi "; break;
+		case 5: cout <<"Nam Muoi "; break;
+		case 6: cout <<"Sau muoi "; break;
+		case 7: cout <<"Bay Muoi "; break;
+		case 8: cout <<"Tam muoi "; break;
+		case 9: cout <<"Chin Muoi "; break;
+		case 0: break;
+	}
+	switch(dv)
+	{
+		case 1: cout <<"mot "; break;
+		case 2: cout <<"Hai  "; break;
+		case 3: cout <<"Ba  "; break;
+		case 4: cout <<"Bon  "; break;
+		case 5: cout <<"Nam  "; break;
+		case 6: cout <<"Sau  "; break;
+		case 7: cout <<"Bay  "; break;
+		case 8: cout <<"Tam  "; break;
+		case 9: cout <<"Chin  "; break;
+		case 0: break;
+	}
+	return 0;
+}
