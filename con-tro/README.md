e2b3fe7c6ea2caaea7bbda2ebdb0bad00ad5000e
dangdan2807 <dangdan2807@gmail.com>
2019-11-10 02:06:37 +0700

    update con tro

---
+++ con-tro/README.md
@@ -0,0 +1,4 @@
+﻿﻿#Con trỏ trong C++
+- coban_contro: 1 vài thao tác cơ bản với con trỏ để các bạn hiểu về con trỏ hơn. 
+- Bài 1: Sử dụng con trỏ trong C++, bạn hãy viết một chương trình C++ để nhận dữ liệu từ người dùng và tìm giá trị lớn nhất của một tập dữ liệu nội bộ.
+- Bài 2: Viết một chương trình C++ để nhận 5 giá trị nguyên từ bàn phím. 5 giá trị này sẽ được lưu trữ trong một mảng bởi sử dụng một con trỏ. Sau đó, in các phần tử của mảng trên màn hình.
---
+++ con-tro/coban_contro.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int a=10;
+	int *b= &a;
+	int c= *&a;
+	//gán địa chỉ của biến a trên thanh RAM vào  b và in ra b
+	cout << b << endl;
+	 //in ra địa chỉ mà biến b đang giữ
+	cout << "dia chi b dang giu: " << *&b << endl;
+	// in ra giá trị tại địa chỉ được lưu trong biến b
+	// hay in ra giá trị của được lưu trên thanh RAM mà biến b nhận
+	cout << "b= " << *b << endl;
+	//gán giá trị tại địa chỉ của biến a nằm trên thanh RAM cho c
+	cout << c << endl;// a=c;
+	//
+	a=20;
+	//lúc này nếu biến a=20 thì biến b=20 vì biến b đàn giữ địa chỉ biến a tại vùng nhớ trên thanh RAM
+	cout << "b=" << *b << endl;
+	*b=30;
+	//ngoài ra nếu thay đổi giá trị tại địa chỉ mà biến b đang giữ thì nó cũng thay đổi biến a theo vì b đang giữ địa chỉ của biến a tại vùng nhớ
+	cout <<"a= " << a << endl;
+	//biến c không đổi vì nó chỉ giữ giá trị của địa chỉ biến a cũng như c=a
+	cout << "c= " << c <<endl;
+	return 0;
+}
