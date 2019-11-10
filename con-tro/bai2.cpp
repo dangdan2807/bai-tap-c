9fdf9c8b8017287d344af54ede40ac7c2b72f04f
=dangdan2807 <=dangdan2807@gmail.com>
2019-11-10 20:11:42 +0700

    update con-contro/ bai2, 3

---
+++ con-tro/bai2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int mang[5];
+	int *prt = mang;
+	cout <<"\tNhap mang: \n";
+	for(int i=0; i<5; i++){
+		cout <<"Mang[" << i <<"]= ";
+		cin >> *(prt+i);
+	}
+	cout <<"\tXuat mang:\n";
+	for(int i=0; i<5; i++)
+		cout <<"Mang[" << i <<"]= "<< mang[i] << "\n";
+	return 0;
+}
