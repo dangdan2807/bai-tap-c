909704e56bf649d2b2af246e7c0a28d51ec21486
dangdan2807 <dangdan2807@gmail.com>
2019-11-18 00:09:18 +0700

    add co-ban/struct/bai1.cpp

--- co-ban/struct/README.md
+++ co-ban/struct/README.md
@@ -1,3 +1,5 @@
 ﻿# Share bài tập C/C++
 
- - Bài 1:
+ - Bài 1: Viết chương trình Book Entry đơn giản sử dụng Structure trong C++.
+   - Tác giả
+   - Tên sách
---
+++ co-ban/struct/bai1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+struct book
+{
+	char tensach[100];
+	char tacgia[50];
+};
+
+int main()
+{
+	int n;
+	cout <<"Nhap so sach: ";
+	cin >> n;
+	fflush(stdin);
+	//Nhap thong tin sach
+	book sach[n];
+	for(int i=0; i<n; i++){
+		cout <<"Nhap thong tin sach " << i+1<<":\n";
+		cout <<"Nhap ten sach: ";
+		fflush(stdin);
+		gets(sach[i].tensach);
+		cout <<"Nhap ten tac gia: ";
+		fflush(stdin);
+		gets(sach[i].tacgia);
+	}
+	//xuat thong tin sach
+	for(int i=0; i<n; i++){
+		cout <<"Thong tin sach " << i+1<<":\n";
+		cout <<"Ten sach: " <<sach[i].tensach <<"\n";
+		cout <<"Ten tac gia: " <<sach[i].tacgia <<"\n";
+	}
+	return 0;
+}
