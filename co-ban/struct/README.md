1c5bb8a5eb8720ca735efa2ffb36debd20b18b53
dangdan2807 <dangdan2807@gmail.com>
2019-11-18 10:24:22 +0700

    add co-ban/struct/bai2.cpp

--- co-ban/struct/README.md
+++ co-ban/struct/README.md
@@ -1,5 +1,11 @@
 ﻿# Share bài tập C/C++
 
- - Bài 1: Viết chương trình Book Entry đơn giản sử dụng Structure trong C++.
+Bài tập phần struct
+
+ - **Bài 1:** Viết chương trình Book Entry đơn giản sử dụng Structure trong C++.
    - Tác giả
    - Tên sách
+- **Bài 2:** Viết chương trình C++ để cộng hai phân số và hiển thị kết quả. Chương trình của bạn sẽ nhắc người dùng nhập phân số 1 và phân số 2. Tử số và mẫu số được nhập một cách riêng rẽ bởi khoảng trống, như dưới đây:
+   - Nhap phan so 1 (tuso mauso): 1 2
+   - Nhap phan so 1 (tuso mauso): 2 5
+   - Ket qua: 9/10
---
+++ co-ban/struct/bai2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> 
+#include <stdio.h>
+#include <math.h>
+using namespace std; 
+//khái báo kiểu dữ liệu phân số
+struct phanso
+{
+	int tuso;
+	int mauso;
+};
+//khai báo hàm cộng phân số với kiển dữ liệu vừa định nghĩa là phanso
+phanso sum(phanso, phanso);
+
+int main() {
+	int tuso1, tuso2, mauso1, mauso2;
+	cout <<"Nhap phan so thu 1(tu so va mau so): ";
+	cin >>tuso1 >>mauso1;
+	cout <<"Nhap phan so thu 2(tu so va mau so): ";
+	cin >>tuso2 >>mauso2;
+	//tạo phân số
+	phanso f1 ={tuso1, mauso1};
+	phanso f2 ={tuso2, mauso2};
+	phanso ketqua = sum(f1, f2);
+	//xuất kết quả
+	cout <<f1.tuso <<"\\" <<f1.mauso <<" + " <<f2.tuso << "\\" <<f2.mauso <<" = ";
+	cout <<ketqua.tuso <<"\\" <<ketqua.mauso <<"\n";
+	return 0; 
+}
+
+phanso sum(phanso f1, phanso f2)
+{
+	phanso sum= {(f1.tuso * f2.mauso)+(f1.mauso*f2.tuso), (f1.mauso*f2.mauso)};
+	return sum;
+}
