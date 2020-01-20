f9e8ba03f9f7d977842858acf83ef4e41c7e3c78
=dangdan2807 <=dangdan2807@gmail.com>
2020-01-20 13:49:35 +0700

    add: vd3
    add vd3.cpp --> ki-thuat-lap-trinh/d
    '
    '

--- bt-tonghop/chuyen-10-2-c2.cpp
+++ bt-tonghop/chuyen-10-2-c2.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <iostream>
 using namespace std;
 #include <math.h>
--- ki-thuat-lap-trinh/de-quy/vd3.cpp
+++ ki-thuat-lap-trinh/de-quy/vd3.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
+﻿#include <iostream>
 #include <stdio.h>
+#include <cmath>
 using namespace std;
+
+void xuatnhiphan(int n);
+
 int main()
 {
-	
+	int n;
+	cout <<"Nhap so he 10: ";
+	cin >>n;
+	xuatnhiphan(n);
 	return 0;
+}
+
+void xuatnhiphan(int n)
+{
+	int i= 0;
+	long long binary_number = 0;
+	while(n> 0)
+	{
+		binary_number += (n%2) * pow(10, i);
+		i++;
+		n/=2;
+	}
+	cout <<binary_number;
 }
