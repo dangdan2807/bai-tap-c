50e9763b463dd44c15b83be0205a10c8e0026697
Kayn Kuri <45003518+dangdan2807@users.noreply.github.com>
2020-04-21 21:48:31 +0700

    Update vd3.cpp

--- ki-thuat-lap-trinh/de-quy/vd3.cpp
+++ ki-thuat-lap-trinh/de-quy/vd3.cpp
@@ -1,28 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdio.h>
 #include <cmath>
 using namespace std;
+ 
 
-void xuatnhiphan(int n);
+void xuatnhiphan(int n)
+{
+	if (n != 0) 
+	{
+		xuatnhiphan(n / 2);
+		cout << n % 2;
+	}
+}
 
 int main()
 {
 	int n;
-	cout <<"Nhap so he 10: ";
-	cin >>n;
+	cout << "Nhap so he 10: ";
+	cin >> n;
 	xuatnhiphan(n);
 	return 0;
-}
-
-void xuatnhiphan(int n)
-{
-	int i= 0;
-	long long binary_number = 0;
-	while(n> 0)
-	{
-		binary_number += (n%2) * pow(10, i);
-		i++;
-		n/=2;
-	}
-	cout <<binary_number;
 }
