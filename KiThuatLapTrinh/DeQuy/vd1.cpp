c7fc435b406c275d2ee45041fcf883d8ee53ab94
KayJuno <dangdan2807@gmail.com>
2021-03-19 09:49:56 +0700

    update: rename func luyThua

--- KiThuatLapTrinh/DeQuy/vd1.cpp
+++ KiThuatLapTrinh/DeQuy/vd1.cpp
@@ -5,49 +5,53 @@ using namespace std;
 
 long tong(int n);
 long tich(int n);
-double luythua(float a, int n);
+double luyThua(float a, int n);
 
 int main()
 {
 	int n;
-	cout <<"Nhap n: ";
-	do{
-		cin >>n;
-		if(n < 0)
-			cout <<"Du lieu khong hop le. Nhap lai n: ";
-	}while(n < 0);
-	cout <<"Nhap so thuc a: ";
+	cout << "Nhap n: ";
+	do
+	{
+		cin >> n;
+		if (n < 0)
+			cout << "Du lieu khong hop le. Nhap lai n: ";
+	} while (n < 0);
+
+	cout << "Nhap so thuc a: ";
 	float a;
-	cin >>a;
-	a*=1.0;
-	cout <<"\nS = 1 + 2 + ... + n = " <<tong(n);
-	cout <<"\nP = 1 * 2 * ... * n = " <<tich(n);
-	cout <<"\nA = a^n = " <<luythua(a, n);
+	cin >> a;
+	a *= 1.0;
+	cout << "\nS(" << n << ") = " << tong(n);
+	cout << "\nP(" << n << ") = " << tich(n);
+	cout << "\nA(" << a << ", " << n << ") = " << luyThua(a, n);
 	return 0;
 }
 
 long tong(int n)
 {
-	if(n <= 0)
+	if (n <= 0)
 		return n;
-	n += tong(n-1);
+	return n += tong(n - 1);
 }
+
 long tich(int n)
 {
-	if(n <= 1)
+	if (n <= 1)
 		return n;
-	n *= tich(n-1);
+	return n *= tich(n - 1);
 }
-double luythua(float a, int n) 
+
+double luyThua(float a, int n)
 {
-	if(n == 1)
+	if (n == 1)
 		return a;
 	else
 	{
-		double temp = luythua(a ,n/2);
-		if(n % 2 == 0)
+		double temp = luyThua(a, n / 2);
+		if (n % 2 == 0)
 			return temp * temp;
 		else
-			return temp * temp  * a;
+			return temp * temp * a;
 	}
 }
