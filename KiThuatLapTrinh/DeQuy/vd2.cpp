5b5218eaf6b7e7857da0597882f442b3e78d2382
KayJuno <dangdan2807@gmail.com>
2021-03-19 09:50:56 +0700

    update: rename function and fix error print

--- KiThuatLapTrinh/DeQuy/vd2.cpp
+++ KiThuatLapTrinh/DeQuy/vd2.cpp
@@ -2,84 +2,96 @@
 #include <stdio.h>
 using namespace std;
 
-double capsocong(int n, int a, int r);
-double capsonhan(int n, int a, int q);
-double capsocong2(int n, int a, int r);
-double capsonhan2(int n, int a, int q);
+double capSoCong(int n, int a, int r);
+double capSoNhan(int n, int a, int q);
+double capSoCong2(int n, int a, int r);
+double capSoNhan2(int n, int a, int q);
 
 int main()
 {
 	int n, a, r, q;
-	cout <<"Nhap thu n: ";
-	do{
-		cin >>n;
-		if(n < 1)
-			cout <<"Du lieu khong hop le, nhap lai n: ";
-	}while(n < 1);
-	cout <<"Nhap gia tri dau a: ";
-	cin >>a;
-	cout <<"Nhap cong sai r: ";
-	cin >>r;
-	cout <<"Nhap cong boi q: ";
-	cin >>q;
-	cout <<"Day cap so cong :\n";
-	capsocong(n, a, r);
-	cout <<"\nDay cap so nhan:\n";
-	capsonhan(n, a, q);
-	cout <<"\n\n\tPhan khong su dung de quy:\n";
-	cout <<"\nDay cap so cong:\n";
-	capsocong2(n, a, r);
-	cout <<"\nDay cap so nhan:\n";
-	capsonhan2(n, a, r);
+	cout << "Nhap thu n: ";
+	do
+	{
+		cin >> n;
+		if (n < 1)
+			cout << "Du lieu khong hop le, nhap lai n: ";
+	} while (n < 1);
+	cout << "Nhap gia tri dau a: ";
+	cin >> a;
+
+	cout << "Nhap cong sai r: ";
+	cin >> r;
+
+	cout << "Nhap cong boi q: ";
+	cin >> q;
+
+	cout << "Day cap so cong :\n";
+	for (int i = 1; i <= n; i++)
+	{
+		cout << capSoCong(i, a, r) << "\t";
+		if (i % 10 == 0)
+			cout << "\n";
+	}
+
+	cout << "\nDay cap so nhan:\n";
+	for (int i = 1; i <= n; i++)
+	{
+		cout << capSoNhan(i, a, q) << "\t";
+		if (i % 10 == 0)
+			cout << "\n";
+	}
+
+	cout << "\n\nPHAN KHONG SU DUNG DE QUY:";
+	cout << "\nDay cap so cong:\n";
+	capSoCong2(n, a, r);
+	cout << "\nDay cap so nhan:\n";
+	capSoNhan2(n, a, q);
 	return 0;
 }
 
-double capsocong(int n, int a, int r)
+double capSoCong(int n, int a, int r)
 {
-	if(n >= 2)
+	if (n == 1)
+		return a;
+	else if (n >= 2)
 	{
 		a += r;
-		capsocong(n-1, a, r);
-	}
-	else if(n == 1)
-		return a;
-	cout << a <<"\t";
-	if((n-1) % 10 == 0)
-		cout <<"\n";
-}
-double capsonhan(int n, int a, int q)
-{
-	if(n == 1)
-		return a;
-	else if(n >= 2)
-	{
-		a *= q;
-		capsonhan(n-1, a, q);
-	}
-	cout << a <<"\t";
-	if((n-1) % 10 == 0)
-		cout <<"\n";
+		return capSoCong(n - 1, a, r);
+	} 
 }
 
-double capsocong2(int n, int a, int r)
+double capSoNhan(int n, int a, int q)
 {
-	for(int i = 1; i < n; i++)
+	if (n == 1)
+		return a;
+	else if (n >= 2)
 	{
+		a *= q;
+		return capSoNhan(n - 1, a, q);
+	}
+}
+
+double capSoCong2(int n, int a, int r)
+{
+	for (int i = 1; i <= n; i++)
+	{
+		cout << a << "\t";
 		a += r;
-		cout <<a <<"\t";
-		if(i % 10 == 0)
-			cout <<"\n";
+		if (i % 10 == 0)
+			cout << "\n";
 	}
 	return a;
 }
-double capsonhan2(int n, int a, int q)
+
+double capSoNhan2(int n, int a, int q)
 {
-	for(int i = 1; i< n; i++)
+	for (int i = 1; i <= n; i++)
 	{
+		cout << a << "\t";
 		a *= q;
-		cout <<a <<"\t";
-		if(i % 10 == 0)
-			cout <<"\n";
+		if (i % 10 == 0)
+			cout << "\n";
 	}
 	return a;
 }
