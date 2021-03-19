c3a2ea038155e04fff109e77a8030d5d832eb686
KayJuno <dangdan2807@gmail.com>
2021-03-19 10:05:03 +0700

    update: thêm điều kiện đầu vào của input

--- KiThuatLapTrinh/DeQuy/vd3.cpp
+++ KiThuatLapTrinh/DeQuy/vd3.cpp
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <cmath>
 using namespace std;
- 
 
 void xuatnhiphan(int n)
 {
-	if (n != 0) 
+	if (n != 0)
 	{
 		xuatnhiphan(n / 2);
 		cout << n % 2;
@@ -17,7 +16,13 @@ int main()
 {
 	int n;
 	cout << "Nhap so he 10: ";
-	cin >> n;
+	do
+	{
+		cin >> n;
+		if(n < 0)
+			cout << "n >= 0\nNhap lai: ";
+	} while (n < 0);
+
 	xuatnhiphan(n);
 	return 0;
 }
