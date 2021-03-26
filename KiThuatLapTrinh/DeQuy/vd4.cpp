7e50da3009df9ddac7ad51318fc20cd37143242d
KayJuno <dangdan2807@gmail.com>
2021-03-26 09:47:31 +0700

    update: tối ưu code

--- KiThuatLapTrinh/DeQuy/vd4.cpp
+++ KiThuatLapTrinh/DeQuy/vd4.cpp
@@ -2,49 +2,63 @@
 #include <string>
 using namespace std;
 
-void nhap_dl(float& a);
+void input(float &a);
+void inputNam(float &a);
 float lai_kep(float y, int n);
-
 
 int main()
 {
 	float von, nam;
 	cout << "Nhap von: ";
-	nhap_dl(von);
+	input(von);
 	cout << "Nhap so nam: ";
-	nhap_dl(nam);
+	inputNam(nam);
 	float tien = lai_kep(von, nam);
-	//xuất ra số nếu số quá lớn (số --> chuỗi)
-	string temp = to_string(tien);
-	cout << "Ket qua: " << temp;
+	cout << "Ket qua: " << tien;
 	return 0;
 }
 
-
-void nhap_dl(float& a)
+void input(float &a)
 {
-	do {
+	do
+	{
 		cin >> a;
-		//don sach bo nho dem khi nhap sai -> khong bi tran bo nho
 		if (cin.fail())
 		{
 			cin.clear();
 			cin.ignore(32767, '\n');
 		}
-		//xuat de nguoi dung biet da nhap sai
 		if (a <= 0)
-			cout << "Du lieu khong hop le, nhap lai: ";
+			cout << "Gia tri nhap khong duoc <= 0, nhap lai: ";
 	} while (a <= 0);
+}
+
+void inputNam(float &a)
+{
+	do
+	{
+		cin >> a;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(32767, '\n');
+		}
+		if (a <= 0)
+			cout << "Gia tri nhap khong duoc <= 0, nhap lai: ";
+		else if (a > 100)
+			cout << "Khong duoc lon hon 100 nam, nhap lai: ";
+	} while (a <= 0 || a > 100);
 }
 
 float lai_kep(float y, int n)
 {
-	// lai suat hang nam
-	float lai_suat = 0.07;
+	float laiSuat = 0.07;
 	if (n == 1)
-		return y * (1 + lai_suat);
-	else if(n >= 2) {
-		y *= (1 + lai_suat);
+		return y * (1 + laiSuat);
+	else
+	{
+		y *= (1 + laiSuat);
 		return lai_kep(y, --n);
 	}
 }
+
--- KiThuatLapTrinh/DeQuy/vd5.cpp
+++ KiThuatLapTrinh/DeQuy/vd5.cpp
@@ -11,58 +11,53 @@ int max_rec(int Q[], int n);
 int min_rec(int Q[], int n);
 long sum(int Q[], int n);
 
-
 int main()
 {
 	int n;
-	int mang[100];
-	input(mang, n);
-	output(mang, n);
-	long sum_dq = sum_rec(mang, n);
-	cout <<"\n\nTong cua mang(de quy): " <<sum_dq;
-	long sum_kdq = sum(mang, n);
-	cout <<"\nTong cua mang(khong de quy): " <<sum_kdq;
-	cout <<endl;
-	cout <<"Gia tri lon nhat cua mang: " <<max_rec(mang, n);
-	cout <<endl;
-	cout <<"Gia tri nho nhat cua mang: " <<min_rec(mang, n);
-	cout <<"\n";
+	int arr[100];
+	input(arr, n);
+	output(arr, n);
+	long sum_dq = sum_rec(arr, n);
+	cout << "\n\nTong cua mang (de quy): " << sum_dq << endl;
+	long sum_kdq = sum(arr, n);
+	cout << "Tong cua mang (khong de quy): " << sum_kdq << endl;
+	cout << "Gia tri lon nhat cua mang: " << max_rec(arr, n) << endl;
+	cout << "Gia tri nho nhat cua mang: " << min_rec(arr, n) << endl;
 	return 0;
 }
 
-
 void input(int Q[], int &n)
 {
-	cout <<"\tKHOI TAO MANG TU DONG.";
+	cout << "\tKHOI TAO MANG TU DONG.";
 	srand(time(NULL));
-	n = 1 + rand()%99;
-	cout <<"\nSo luong phan tu cua mang(random) la: " << n;
+	n = 1 + rand() % 99;
+	cout << "\nSo luong phan tu cua mang(random) la: " << n;
 	int a = pow(10, 6);
 	int b = pow(10, 4);
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		Q[i] = b + rand()%(a - b);
+		Q[i] = b + rand() % (a - b);
 	}
-	cout <<"\nKhoi tao mang hoan tat";
+	cout << "\nKhoi tao mang hoan tat";
 }
 
 void output(int Q[], int n)
 {
-	cout <<"\n\tXUAT MANG:\n";
-	for(int i = 0; i < n; i++)
+	cout << "\n\tXUAT MANG:\n";
+	for (int i = 0; i < n; i++)
 	{
-		cout <<Q[i] <<"\t";
-		if(i+1 % 5 == 0)
-			cout <<"\n";
+		cout << Q[i] << "\t";
+		if (i + 1 % 5 == 0)
+			cout << "\n";
 	}
 }
 
 long sum_rec(int Q[], int n)
 {
 	long sum = 0;
-	if(n < 1)
+	if (n < 1)
 		return sum;
-	sum += (Q[n-1] + sum_rec(Q, n-1));
+	return (sum += (Q[n - 1] + sum_rec(Q, n - 1)));
 }
 
 int max_rec(int A[], int n)
@@ -73,10 +68,10 @@ int max_rec(int A[], int n)
 		return A[0];
 	else
 	{
-		if (A[n-1] > max_rec(A, n-1))
-			return A[n-1];
+		if (A[n - 1] > max_rec(A, n - 1))
+			return A[n - 1];
 		else
-			return max_rec(A, n-1);
+			return max_rec(A, n - 1);
 	}
 }
 
@@ -88,17 +83,17 @@ int min_rec(int A[], int n)
 		return A[0];
 	else
 	{
-		if (A[n-1] < min_rec(A, n-1))
-			return A[n-1];
+		if (A[n - 1] < min_rec(A, n - 1))
+			return A[n - 1];
 		else
-			return min_rec(A, n-1);
+			return min_rec(A, n - 1);
 	}
 }
 
 long sum(int Q[], int n)
 {
 	long sum = 0;
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 		sum += Q[i];
 	return sum;
 }
